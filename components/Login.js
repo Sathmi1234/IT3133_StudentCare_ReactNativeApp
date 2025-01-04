a0232aea30768400f5048d5fc901201475dfc8c1
Sathmi1234 <devsadini@gmail.com>
2025-01-04 12:28:40 +0530

    Designed login with text input fields

--- components/Login.js
+++ components/Login.js
@@ -1,24 +1,35 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, PaperProvider, Text } from "react-native-paper";
+import { Button, PaperProvider, Text, TextInput } from "react-native-paper";
 import { Image, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native";
 
 export default function Login() {
   const navigation = useNavigation();
   return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Image
-            style={styles.image}
-            source={require('../assets/Logo.png')}/>
-        <Text style={styles.text}>Student Login</Text>
-        <Button mode="contained"
-          onPress={() => navigation.navigate('Main')}
-          labelStyle={styles.buttonText}
-        >
-          Login
-        </Button>
-      </View>
-    </PaperProvider>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+            <View style={styles.container}>
+                <View style={styles.banner}>
+                    <Image
+                        style={styles.image}
+                        source={require('../assets/Logo.png')}/>
+                </View>
+                <View style={styles.title}>
+                    <Text style={styles.text}>Student Login</Text>
+                </View>
+                <View style={styles.inputfield}>
+                    <TextInput label="Username" mode="outlined" activeOutlineColor="#63625b"/>
+                    <TextInput label="Password" mode="outlined" activeOutlineColor="#63625b"/>
+                </View>
+                <View>
+                    <Button mode="contained"
+                    onPress={() => navigation.navigate('Main')}
+                    labelStyle={styles.buttonText}
+                    >
+                    Login
+                    </Button>
+                </View>  
+            </View>
+        </ScrollView>
   );
 }
 
@@ -29,6 +40,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
+  inputfield:{
+    flex:1
+  },
+  banner:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title:{
+    flex:1
+  },
   text: {
     color: "black",
     fontSize: 20,
@@ -38,9 +60,13 @@ const styles = StyleSheet.create({
     color: "black",
   },
   image:{
+    flex:1,
     width:250,
     height:65,
     top:10,
     position:'absolute'
-  }
+  },
+  scrollView: {
+    flexGrow: 1,
+},
 });
