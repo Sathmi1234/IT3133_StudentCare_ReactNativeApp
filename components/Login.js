ab8fa00de7229462fb5e014c6e21d07f458ddfa8
Sathmi1234 <devsadini@gmail.com>
2025-01-04 14:38:11 +0530

    created login function

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, PaperProvider, Text, TextInput } from "react-native-paper";
-import { Image, StyleSheet, View } from "react-native";
+import { Alert, Image, StyleSheet, View } from "react-native";
 import { ScrollView } from "react-native";
 import { useState } from "react";
+import { students } from '../assets/StudentsDb'
 
 export default function Login() {
   const navigation = useNavigation();
   const [username,setUsername]=useState();
   const [password,setPassword]=useState();
+
+  const handleLogin=()=>{
+    const user=students.find(
+        (student)=>student.username === username && student.password===password
+    );
+    if(user){
+        navigation.navigate('Main');
+    }else{
+        Alert.alert('Error', 'Error Login');
+    }
+  }
 
   return (
         <ScrollView contentContainerStyle={styles.scrollView}>
@@ -21,10 +33,22 @@ export default function Login() {
                     <Text variant="displayMedium" style={styles.text}>Student Login</Text>
                 </View>
                 <View style={styles.inputfields}>
-                    <TextInput label="Username" mode="outlined" activeOutlineColor="#63625b"/>
-                    <TextInput label="Password" mode="outlined" activeOutlineColor="#63625b"/>
+                    <TextInput 
+                        label="Username" 
+                        mode="outlined" 
+                        activeOutlineColor="#63625b"
+                        value={username}
+                        onChangeText={setUsername}
+                    />
+                    <TextInput 
+                        label="Password" 
+                        mode="outlined" 
+                        activeOutlineColor="#63625b"
+                        value={password}
+                        onChangeText={setPassword}
+                    />
                     <Button style={styles.button} mode="contained"
-                        onPress={() => navigation.navigate('Main')}
+                        onPress={handleLogin}
                         labelStyle={styles.buttonText}
                     >
                     Login
