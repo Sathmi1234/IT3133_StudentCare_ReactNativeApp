302707b190904dd0e9f1c92aee3f34799806c7ea
Sathmi1234 <devsadini@gmail.com>
2025-01-04 19:49:21 +0530

    Added information cards

--- components/Profile.js
+++ components/Profile.js
@@ -24,18 +24,35 @@ export default function Profile({ user }) {
                 </Card.Content>
             </Card>
         </View>
-        <View style={styles.imageCard}>
-            <View>
-            <Image
-                style={styles.propic}
-                source={user.profile_pic}
-            />
-            </View>
+        <View style={styles.card}>
+            <Card style={{backgroundColor:"#ffffff"}}>
+                <Card.Content>
+                    <Text variant="titleLarge" style={styles.cardtitletext}>
+                        Contact Information
+                    </Text>
+                    <Text variant="bodyMedium">Email: {user.email}</Text>
+                    <Text variant="bodyMedium">Phone: {user.phone}</Text>
+                    <Text variant="bodyMedium">Address: {user.address}</Text>
+                </Card.Content>
+            </Card>
+        </View>
+        <View style={styles.card}>
+            <Card style={{backgroundColor:"#ffffff"}}>
+                <Card.Content>
+                    <Text variant="titleLarge" style={styles.cardtitletext}>
+                        Biological Information
+                    </Text>
+                    <Text variant="bodyMedium">Gender: {user.gender}</Text>
+                    <Text variant="bodyMedium">Age: {user.age}</Text>
+                    <Text variant="bodyMedium">Blood Group: {user.blood_group}</Text>
+                </Card.Content>
+            </Card>
         </View>
         <View style={styles.footer}>
-            <Text style={styles.footerText}>UoV © 2025</Text>
+            <Text style={{color: "#ffffff"}}>UoV © 2025</Text>
         </View>
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
