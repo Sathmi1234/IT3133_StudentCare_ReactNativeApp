9226825121737ca20a9070e302b7633fdb7fa122
Sathmi1234 <devsadini@gmail.com>
2025-01-04 15:07:53 +0530

    Created the sructure of the main component

---
+++ components/Main.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+
+const ProfileRoute = () => <Text>Profile</Text>;
+
+const CoursesRoute = () => <Text>Courses</Text>;
+
+const SubjectsRoute = () => <Text>Subjects</Text>;
+
+export default function Main(){
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline'},
+    { key: 'courses', title: 'Courses', focusedIcon: 'school' , unfocusedIcon: 'school-outline'},
+    { key: 'subjects', title: 'Subjects', focusedIcon: 'book-open' , unfocusedIcon: 'book-open-outline'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    profile: ProfileRoute,
+    courses: CoursesRoute,
+    subjects: SubjectsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
+
