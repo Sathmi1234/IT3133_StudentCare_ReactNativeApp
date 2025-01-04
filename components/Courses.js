810a3e287ca4c1833c656569f16c34034ec7f854
Sathmi1234 <devsadini@gmail.com>
2025-01-04 15:25:25 +0530

    Created Profile, Courses, Subject components

---
+++ components/Courses.js
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function Courses(){
+    return(
+        <View><Text>This is Courses Screen</Text></View>
+    )
+}
